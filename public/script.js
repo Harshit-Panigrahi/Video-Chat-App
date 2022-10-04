676a7139d6d3b1bf0955f9ba37d0d10fb6db1984
Harshit Panigrahi <61592605+Harshit-Panigrahi@users.noreply.github.com>
2022-10-04 17:18:15 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,52 +1,72 @@
-const socket = io("/")
+const socket = io("/");
 
 let peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "443",
-})
+});
 
 const user = prompt("Enter your name: ");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
 let myStream;
-navigator.mediaDevices.getUserMedia({
-  audio: true,
-  video: true,
-}).then((stream)=>{
-  myStream = stream;
-  addVideoStream(myVideo, stream)
-});
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: false,
+  })
+  .then((stream) => {
+    myStream = stream;
+    addStream(myVideo, stream);
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+    peer.on("call", (call)=>{
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addStream(video, userVideoStream);
+      })
+    })
+  });
 
-function addVideoStream(video, stream) {
+function connectToNewUser(userId, stream) {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addStream(video, userVideoStream);
+  });
+}
+
+function addStream(video, stream) {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
     $("#video-grid").append(video);
-  })
+  });
 }
 
-$(function(){
-  $("#show_chat").click(function() {
+$(function () {
+  $("#show_chat").click(function () {
     $(".left-window").css("display", "none");
     $(".right-window").css("display", "block");
     $("#header_back").css("display", "block");
-  })
-  $("#header_back").click(function() {
+  });
+  $("#header_back").click(function () {
     $(".left-window").css("display", "block");
     $(".right-window").css("display", "none");
     $("#header_back").css("display", "none");
-  })
+  });
 
-  $("#send").click(function() {
-    if($("#chat_message").val().length !== 0){
+  $("#send").click(function () {
+    if ($("#chat_message").val().length !== 0) {
       socket.emit("message", $("#chat_message").val());
       $("#chat_message").val("");
     }
-  })
-  $("#chat_message").keydown(function(e){
-    if(e.key == "Enter" && $("#chat_message").val().length !== 0) {
+  });
+  $("#chat_message").keydown(function (e) {
+    if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
       socket.emit("message", $("#chat_message").val());
       $("#chat_message").val("");
     }
@@ -65,6 +85,6 @@ $(function(){
         </b>
         <span>${msg}</span>
       </div>`
-    )
-  })
-})
+    );
+  });
+});
