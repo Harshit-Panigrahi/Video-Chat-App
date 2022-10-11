3ab4834f1486973ef2dd5def6aa5a65cbe17bc5b
Harshit Panigrahi <61592605+Harshit-Panigrahi@users.noreply.github.com>
2022-10-11 17:39:17 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,37 +1,40 @@
-const socket = io("/")
+const { application } = require("express");
+
+const socket = io("/");
 
 let peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "443",
-})
+});
 
 const user = prompt("Enter your name: ");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
 let myStream;
-navigator.mediaDevices.getUserMedia({
-  audio: true,
-  video: true,
-})
-.then((stream)=>{
-  myStream = stream;
-  addStream(myVideo, stream);
-  socket.on("user-connected", (userId) => {
-    console.log("user connected")
-    connectToNewUser(userId, stream);
-  });
-  peer.on("call", (call) => {
-    console.log("Call");
-    call.answer(stream);
-    const video = document.createElement("video");
-    call.on("stream", (userVideoStream) => {
-      console.log("stream")
-      addStream(video, userVideoStream);
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    myStream = stream;
+    addStream(myVideo, stream);
+    socket.on("user-connected", (userId) => {
+      console.log("user connected");
+      connectToNewUser(userId, stream);
+    });
+    peer.on("call", (call) => {
+      console.log("Call");
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        console.log("stream");
+        addStream(video, userVideoStream);
+      });
     });
   });
-});
 
 function connectToNewUser(userId, stream) {
   console.log("connecting to new user");
@@ -47,48 +50,68 @@ function addStream(video, stream) {
   video.addEventListener("loadedmetadata", () => {
     video.play();
     $("#video-grid").append(video);
-  })
+  });
 }
 
-$(function(){
-  $("#show_chat").click(function() {
+$(function () {
+  $("#show_chat").click(function () {
     $(".left-window").css("display", "none");
     $(".right-window").css("display", "block");
     $("#header_back").css("display", "block");
-  })
-  $("#header_back").click(function() {
+  });
+  $("#header_back").click(function () {
     $(".left-window").css("display", "block");
     $(".right-window").css("display", "none");
     $("#header_back").css("display", "none");
-  })
+  });
 
-  $("#send").click(function() {
-    if($("#chat_message").val().length !== 0){
-      socket.emit("message", $("#chat_message").val());
-      $("#chat_message").val("");
-    }
-  })
-  $("#chat_message").keydown(function(e){
-    if(e.key == "Enter" && $("#chat_message").val().length !== 0) {
+  $("#send").click(function () {
+    if ($("#chat_message").val().length !== 0) {
       socket.emit("message", $("#chat_message").val());
       $("#chat_message").val("");
     }
   });
+  $("#chat_message").keydown(function (e) {
+    if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+      socket.emit("message", $("#chat_message").val());
+      $("#chat_message").val("");
+    }
+  });
+  $("#inv_btn").click(function () {
+    to = prompt("Enter the email address to invite:");
+    let data = {
+      url: window.location.href,
+      to: to,
+    };
+    $.ajax({
+      url: "/send-mail",
+      type: "post",
+      data: JSON.stringify(data),
+      dataType: "json",
+      contentType: "application/json",
+      success: function (result) {
+        alert("Invite sent!");
+      },
+      error: function (result) {
+        console.log(result.responseJSON);
+      },
+    });
+  });
 
-  $("#mute_audio").click(function() {
+  $("#mute_audio").click(function () {
     const enabled = myStream.getAudioTracks()[0].enabled;
     if (enabled) {
       myStream.getAudioTracks()[0].enabled = false;
       html = `<i class="fa fa-microphone-slash"></i>`;
       $("#mute_audio").toggleClass("background_red");
-      $("#mute_audio").html(html)
+      $("#mute_audio").html(html);
     } else {
       myStream.getAudioTracks()[0].enabled = true;
       html = `<i class="fa fa-microphone"></i>`;
       $("#mute_audio").toggleClass("background_red");
       $("#mute_audio").html(html);
     }
-  })
+  });
 
   $("#stop_video").click(function () {
     const enabled = myStream.getVideoTracks()[0].enabled;
@@ -118,6 +141,6 @@ $(function(){
         </b>
         <span>${msg}</span>
       </div>`
-    )
-  })
-})
+    );
+  });
+});
--- server.js
+++ server.js
@@ -2,33 +2,63 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
+let nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  port: 465,
+  host: "smtp.gmail.com",
+  auth: {
+    user: "harshit.whitehat@gmail.com",
+    pass: "sdrckwcdvrbwgcgh",
+  },
+  secure: true,
+});
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 const { v4: uuidv4 } = require("uuid");
-const io = require("socket.io")(server, { cors: { origin: '*' } })
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, { debug: true });
 app.use("/peerjs", peerServer);
 
-
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
-})
+});
 
 app.get("/:room", (req, res) => {
-  res.render("index", { roomId: req.params.room })
-})
+  res.render("index", { roomId: req.params.room });
+});
+
+app.post("/send-mail", (req, res) => {
+  const to = req.body.to;
+  const url = req.body.url;
+  const mailData = {
+    from: "harshit.whitehat@gmail.com",
+    to: to,
+    subject: "Requesting you to join video chat",
+    html: `<p>Hey there,</p><p>Please join me for a video chat: ${url}</p>`,
+  };
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).send({
+      msg: "Invitation successfully sent",
+      msgId: info.messageId,
+    })
+  });
+});
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, username) => {
-    socket.join(roomId)
+    socket.join(roomId);
     io.to(roomId).emit("user-connected", userId);
     socket.on("message", (msg) => {
       io.to(roomId).emit("createMessage", msg, username);
-    })
-  })
-})
+    });
+  });
+});
 
 server.listen(process.env.PORT || 3030);
